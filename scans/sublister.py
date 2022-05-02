fb14ff6a0c6fdae5b34c14d061c21cb86914fc24
Tuutaans <69729332+Tuutaans@users.noreply.github.com>
2022-05-02 23:26:17 +0545

    bugs fixed

--- scans/sublister.py
+++ scans/sublister.py
@@ -2,5 +2,5 @@ import subprocess
 
 def sublist(domain):
     print("Sublist3r is starting")
-    command = f"sublist3er -d {domain} >> sublister.txt )"
-    com.subprocess(command,shell=True)
+    command = f"sublist3er -d {domain} >> sublister.txt"
+    subprocess.call(command,shell=True)
