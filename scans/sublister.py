6c80b03276d68d430a1803192d22f6dafd5ba125
Tuutaans <69729332+Tuutaans@users.noreply.github.com>
2022-05-02 23:29:48 +0545

    incorrect tool name

--- scans/sublister.py
+++ scans/sublister.py
@@ -2,5 +2,5 @@ import subprocess
 
 def sublist(domain):
     print("Sublist3r is starting")
-    command = f"sublist3er -d {domain} >> sublister.txt"
+    command = f"sublist3r -d {domain} >> sublister.txt"
     subprocess.call(command,shell=True)
